e68a9c8cf321a9178b8677cd3fc40479f6e5f49f
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-02-05 16:16:48 -0300

    agrege querys

--- src/main/java/com/desafio/purchaseOrder/repository/PurchaseOrderRepository.java
+++ src/main/java/com/desafio/purchaseOrder/repository/PurchaseOrderRepository.java
@@ -1,11 +1,14 @@
 package com.desafio.purchaseOrder.repository;
 
-import com.desafio.purchaseOrder.dto.ArticleDTO;
 import com.desafio.purchaseOrder.dto.ArticleOrderDTO;
 import com.desafio.purchaseOrder.dto.requestDTO.PurchaseRequestDTO;
+
+import java.util.ArrayList;
 
 public interface PurchaseOrderRepository {
 
     void addOrder(PurchaseRequestDTO order);
+    boolean existOrderUser(String user);
+    ArrayList<ArticleOrderDTO> getOrderByUser(String user);
 
 }
--- src/main/java/com/desafio/purchaseOrder/repository/impl/PurchaseOrderRepositoryImpl.java
+++ src/main/java/com/desafio/purchaseOrder/repository/impl/PurchaseOrderRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.desafio.purchaseOrder.repository.impl;
 
-import com.desafio.purchaseOrder.dto.ArticleDTO;
 import com.desafio.purchaseOrder.dto.ArticleOrderDTO;
 import com.desafio.purchaseOrder.dto.requestDTO.PurchaseRequestDTO;
 import com.desafio.purchaseOrder.repository.PurchaseOrderRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -33,6 +31,16 @@ public class PurchaseOrderRepositoryImpl implements PurchaseOrderRepository {
             keyValueDB.put(order.getUserName(), order.getArticlesOrder());
         }
 
+    }
+
+    @Override
+    public boolean existOrderUser(String user) {
+        return keyValueDB.containsKey(user);
+    }
+
+    @Override
+    public ArrayList<ArticleOrderDTO> getOrderByUser(String user) {
+        return keyValueDB.get(user);
     }
 
 }
