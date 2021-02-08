e1aa20dd7d85c1fe61b332c42b13b952831bb28c
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-02-08 08:43:14 -0300

    rest controller fix bugs

--- src/main/java/com/desafio/purchaseOrder/controller/PurchaseOrderController.java
+++ src/main/java/com/desafio/purchaseOrder/controller/PurchaseOrderController.java
@@ -1,2 +1,24 @@
-package com.desafio.purchaseOrder.controller;public class PurchaseOrderController {
+package com.desafio.purchaseOrder.controller;
+
+import com.desafio.purchaseOrder.dto.requestDTO.PurchaseRequestDTO;
+import com.desafio.purchaseOrder.dto.responseDTO.PurchaseOrderResponseDTO;
+import com.desafio.purchaseOrder.service.impl.PurchaseOrderServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/purchase-request")
+public class PurchaseOrderController {
+    private final PurchaseOrderServiceImpl purchaseOrderService;
+
+    public PurchaseOrderController(PurchaseOrderServiceImpl purchaseOrderService) {
+        this.purchaseOrderService = purchaseOrderService;
+    }
+
+    @PostMapping()
+    PurchaseOrderResponseDTO purchaseOrderRequest(@RequestBody PurchaseRequestDTO purchaseOrderRequest){
+        return purchaseOrderService.addPurchaseOrder(purchaseOrderRequest);
+    }
 }
