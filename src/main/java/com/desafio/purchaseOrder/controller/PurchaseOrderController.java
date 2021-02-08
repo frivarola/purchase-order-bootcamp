f584c0c6186191d645acf713b1505bf25e48ef9d
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-02-08 12:05:42 -0300

    add exception en controller

--- src/main/java/com/desafio/purchaseOrder/controller/PurchaseOrderController.java
+++ src/main/java/com/desafio/purchaseOrder/controller/PurchaseOrderController.java
@@ -2,11 +2,14 @@ package com.desafio.purchaseOrder.controller;
 
 import com.desafio.purchaseOrder.dto.requestDTO.PurchaseRequestDTO;
 import com.desafio.purchaseOrder.dto.responseDTO.PurchaseOrderResponseDTO;
+import com.desafio.purchaseOrder.exceptions.PurchaseOrderException;
 import com.desafio.purchaseOrder.service.impl.PurchaseOrderServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/api/v1/purchase-request")
@@ -19,6 +22,10 @@ public class PurchaseOrderController {
 
     @PostMapping()
     PurchaseOrderResponseDTO purchaseOrderRequest(@RequestBody PurchaseRequestDTO purchaseOrderRequest){
-        return purchaseOrderService.addPurchaseOrder(purchaseOrderRequest);
+        try {
+            return purchaseOrderService.addPurchaseOrder(purchaseOrderRequest);
+        } catch (PurchaseOrderException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encontro articulo para el id provisto.");
+        }
     }
 }
